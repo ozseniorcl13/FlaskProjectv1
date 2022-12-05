b9a83d43a23b68da72d445a3dd647ae3220f741e
ozseniorcl13 <seniorcl13@gmail.com>
2022-12-04 20:35:11 -0400

    feat: update one note

--- notes.py
+++ notes.py
@@ -24,3 +24,18 @@ def delete(note_id):
             404,
             f"Note with ID {note_id} not found"
         )
+        
+def update(note_id, note):
+    existing_note = Note.query.get(note_id)
+    
+    if existing_note:
+        update_note = note_schema.load(note, session = db.session)
+        existing_note.content = update_note.content
+        db.session.merge(existing_note)
+        db.session.commit()
+        return note_schema.dump(existing_note), 201
+    else:
+        abort(
+            404,
+            f"Person with last name {lname} not found"
+        )
