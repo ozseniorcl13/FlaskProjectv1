daef5cb2723795feaad24d35050f1f46c5c6256f
ozseniorcl13 <seniorcl13@gmail.com>
2022-12-04 20:22:36 -0400

    feat: delete one note

--- notes.py
+++ notes.py
@@ -11,3 +11,16 @@ def read_one(note_id):
         abort(
             404, f"Note with ID {note_id} not found"
         )
+        
+def delete(note_id):
+    existing_note = Note.query.filter(Note.id == note_id).one_or_none()
+    
+    if existing_note:
+        db.session.delete(existing_note)
+        db.session.commit()
+        return make_response(f'{note_id} successfully deleted', 200)
+    else:
+        abort(
+            404,
+            f"Note with ID {note_id} not found"
+        )
